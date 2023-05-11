99909b7499417b04a51cc8e92d5b2b020e5f0ead
Júlia Valls Moix <juliavalls20@gmail.com>
2023-05-11 14:04:28 +0200

    Continue with what Oscar has told me in Discord

--- fm_45_rest-countries-api-master/fm_45_rest-countries-api-master/js/main.js
+++ fm_45_rest-countries-api-master/fm_45_rest-countries-api-master/js/main.js
@@ -12,3 +12,40 @@ function init() {
 }
 
 window.onload = init();
+
+fetch('https://restcountries.com/v3.1/all')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+
+    data.sort((element1, element2) => {
+      const nameA = element1.name.common.toLowerCase();
+      const nameB = element2.name.common.toLowerCase();
+
+      return nameA.localeCompare(nameB);
+    });
+
+    data.forEach(element => {
+      
+      console.log(element.name.common);
+
+      const countryDiv = document.createElement('div');
+      countryDiv.innerHTML = `<button class="country-flag" aria-label="open Afghanistan details" style="background-image: url(${element.flags.png});"></button>
+          
+      <div class="country-info-box">
+        <h2 class="country-name"> ${element.name.common} </h2>
+        <p>
+          <span>Population: </span><span> ${element.population} </span></p>
+          <p><span>Region: </span><span> ${element.region} </span></p>
+          <p><span>Capital: </span><span> ${element.capital} </span></p>
+        </div>`;
+      
+      document.querySelector('#countries-selection-box').appendChild(countryDiv);
+      countryDiv.classList.add('card');
+
+      document.querySelector('#input-field-country').value;
+
+
+    });
+
+  });
